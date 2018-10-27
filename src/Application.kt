c4bf2957d908f4f6ba5564177e332a6c359bab35
amichal2 <andrzej.michalski.3@gmail.com>
2018-10-27 19:08:40 +0100

    Initial ktor application structure

---
+++ src/Application.kt
@@ -0,0 +1,27 @@
+package com.amichal2.brief
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.Routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+
+    install(CallLogging)
+
+    install(Routing) {
+        get("/") {
+            call.respondText("Response text html", ContentType.Text.Html)
+        }
+    }
+}
