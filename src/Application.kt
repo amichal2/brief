7136902576822a23385bb415dfe652492a3cc168
amichal2 <andrzej.michalski.3@gmail.com>
2018-10-28 18:01:43 +0000

    Return sample html and json responses

--- src/Application.kt
+++ src/Application.kt
@@ -1,14 +1,19 @@
 package com.amichal2.brief
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.features.CallLogging
-import io.ktor.features.DefaultHeaders
+import io.ktor.features.*
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
+import io.ktor.request.httpVersion
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.Routing
+
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -19,9 +24,28 @@ fun Application.module(testing: Boolean = false) {
 
     install(CallLogging)
 
+    install(ForwardedHeaderSupport)
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
     install(Routing) {
         get("/") {
-            call.respondText("Response text html", ContentType.Text.Html)
+            val httpVersion = call.request.httpVersion
+            val clientIp = call.request.origin.remoteHost
+            val paramValue = call.request.queryParameters["param"]
+            call.respondText(
+                "Request from $clientIp, http version $httpVersion, param value $paramValue",
+                ContentType.Text.Html
+            )
+        }
+        get("/data") {
+            call.respond(HttpStatusCode.Created, ResponseInfo("ok", 200))
         }
     }
 }
+
+data class ResponseInfo(val info: String, val status: Int)
