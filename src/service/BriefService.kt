3e80a33e582c1714c0b3e873f865f4c2b744e4bb
amichal2 <andrzej.michalski.3@gmail.com>
2019-06-06 23:56:17 +0100

    Return content as String

--- src/service/BriefService.kt
+++ src/service/BriefService.kt
@@ -1,16 +1,15 @@
 package com.amichal2.brief.service
 
 import com.amichal2.brief.client.BriefClientImpl
-import com.amichal2.brief.model.ContentResponse
 
 interface BriefService {
-    suspend fun getContent(): ContentResponse
+    suspend fun getContent(): String
 }
 
 class BriefServiceImpl(private val briefClient: BriefClientImpl) : BriefService {
 
-    override suspend fun getContent(): ContentResponse {
-        val article = briefClient.getGuardianContent().filter { it.fields.wordcount.toInt() in 100..500 }.first()
-        return ContentResponse(article.fields.headline.toUpperCase() + "    " + article.fields.bodyText)
+    override suspend fun getContent(): String {
+        val article = briefClient.getGuardianContent().first { it.fields.wordcount.toInt() in 100..500 }
+        return article.fields.headline.toUpperCase() + "    " + article.fields.bodyText
     }
 }
